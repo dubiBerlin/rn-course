cce1bf6be29010b8d61fc6a751ea14e768288648
Dubravko <woose@web.de>
2018-05-07 15:48:56 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default class App extends React.Component {
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+
+          <Button
+            title="Add"
+            style={styles.button}
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f2ef5e",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  button: {
+    width: "30%"
+  }
+});
