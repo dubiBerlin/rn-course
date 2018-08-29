3707c5073a245ef1f3426ea15dab9b5dbc7491b8
Dubravko <woose@web.de>
2018-08-29 10:10:39 +0200

    Different Action types and methods created

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,6 @@
+// die Konstanten definieren den ActionType.
+// Man benutzt sie global um Schreibfehler zu verhindern
+export const ADD_PLACE = "ADD_PLACE";
+export const DELETE_PLACE = "DELETE_PLACE";
+export const SELECT_PLACE = "SELECT_PLACE";
+export const DESELECT_PLACE = "DESELECT_PLACE";
---
+++ src/store/actions/index.js
@@ -0,0 +1,4 @@
+
+// index,.js ist die zentrale Stelle um Actions zu exportieren.
+// Wenn man mehrere Action Dateien hat ergibt das Sinn.
+export { addPlace, deletePlace, selectPlace, deselectPlace } from "./places";
