975628f2d659cc4d5a50cdf8088c26be2ccb1cf2
Dubravko <woose@web.de>
2018-09-03 11:37:19 +0200

    styles added to input fields in auth screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 
 import startMainTabs from "../MainTabs/startMainTabs";
 
@@ -10,16 +10,39 @@ class AuthScreen extends Component {
     }
 
     render() {
+        // underlineColorAndroid entfernt die borderbottom im input field in Android
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Please Log In</Text>
-                <Button title="Switch To Login" />
-                <TextInput placeholder="Your E-Mail Address" />
-                <TextInput placeholder="Password" />
-                <TextInput placeholder="Confirm Password" />
-                <Button title="Submit" onPress={this.loginHandler} />
-            </View>
+                <Button title="Switch To Login" style={styles.input} />
+                <View style={styles.inputContainer}>
+                    <TextInput placeholder="Your E-Mail Address" style={styles.input} underlineColorAndroid="transparent" />
+                    <TextInput placeholder="Password" style={styles.input} underlineColorAndroid="transparent" />
+                    <TextInput placeholder="Confirm Password" style={styles.input} underlineColorAndroid="transparent" />
+                </View>
+                <Button title="Submit" onPress={this.loginHandler} style={styles.input} />
+            </View >
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // nimmt die gesamte Seite ein
+        justifyContent: "center", // Zentriert den Inhalt (standard Vertikal)
+        alignItems: "center"
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#eee",
+        padding: 5,
+        margin: 8,
+        borderRadius: 5
+    }
+})
+
 export default AuthScreen;
